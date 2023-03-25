5d3594beee137338497320d2687c60e8d1810b6c
harshraj78 <nharshraj78@gmail.com>
2023-03-25 20:34:31 +0530

    fix: image name

--- src/components/home.js
+++ src/components/home.js
@@ -80,13 +80,13 @@ class Home extends Component {
                 <a name="about"></a>
                 <section className="body-content about-swoc" id="about">
                     <div className="about-content col-lg-7">
-                        <h3>About <span>codeforgood</span></h3>
+                        <h3>About <span>CodeForGood</span></h3>
                         <p>
-                            codeforgood is an exciting hackathon event taking place on 29-230th April. This 12-hour event is 
+                            CodeForGood is an exciting hackathon event taking place on 29-230th April. This 12-hour event is 
                             designed to give attendees the opportunity to explore and learn how hackers build innovative ideas.
                         </p>
                         <p>
-                            At codeforgood, participants will have the chance to work with some of the leading experts in the field 
+                            At CodeForGood, participants will have the chance to work with some of the leading experts in the field 
                             and discover new ways of approaching and building ideas. Whether you're an experienced developer or 
                             just starting out, this event is the perfect opportunity to learn and grow your skills.
                         </p>
